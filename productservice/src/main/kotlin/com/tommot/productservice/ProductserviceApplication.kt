62eaa2ffb7654951c51145b3498d6ddc75146286
Tosin <tosintubi@users.noreply.github.com>
2024-01-01 22:33:43 +0100

    feat(otel): Initial commit

---
+++ productservice/src/main/kotlin/com/tommot/productservice/ProductserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.tommot.productservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProductserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProductserviceApplication>(*args)
+}
---
+++ productservice/src/test/kotlin/com/tommot/productservice/ProductserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tommot.productservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProductserviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
