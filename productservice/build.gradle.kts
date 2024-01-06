64afdc7cd41a5890912069cc378208494956f885
Tosin <tosintubi@users.noreply.github.com>
2024-01-06 23:31:21 +0100

    feat(otel): upgrade image version to 0.0.2-SNAPSHOT

--- productservice/build.gradle.kts
+++ productservice/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "com.tommot"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.2-SNAPSHOT"
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_17
@@ -76,7 +76,7 @@ jib {
 	extraDirectories {
 		paths {
 			path {
-				setFrom(layout.buildDirectory.dir("otelagent"))
+				setFrom(layout.buildDirectory.dir("agent"))
 				into = "/otelagent"
 			}
 		}
