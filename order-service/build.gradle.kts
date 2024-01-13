691ff33c1dd9e19cb90eef02cdb598628ea29562
Tosin <tosintubi@users.noreply.github.com>
2024-01-13 01:02:17 +0100

    chore: temporarily remove boot-starter-data-jpa

--- order-service/build.gradle.kts
+++ order-service/build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+//	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.micrometer:micrometer-tracing-bridge-brave")
