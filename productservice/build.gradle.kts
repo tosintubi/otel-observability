d6320fefcb5c20c0b9721933cd7b5fc741f183dd
Tosin <tosintubi@users.noreply.github.com>
2024-02-18 23:21:27 +0100

    feat(security): add sb security dependency

--- productservice/build.gradle.kts
+++ productservice/build.gradle.kts
@@ -26,12 +26,14 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
 
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
 
 	configurations.create("agent")
 	dependencies {
